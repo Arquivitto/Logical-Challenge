e4ebe6005b6b8d3dbeaffa232e3a83bb19d2656f
Arquivitto <xvitorrocha@gmail.com>
2024-06-29 18:52:19 -0300

    Trying with a function named nameHero.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Seja bem-vindo a Code Land! A terra dos códigos, onde aventureiros podem se tornar lendas!")
+function nameHero(){
+
+    const readline = require('readline');
+    const askName = readline.createInterface({
+        input: process.stdin,
+        output: process.stout
+    });
+
+    askName.question("Qual é o seu nome? ", (heroName) => {
+        console.log("Olá, " + heroName);
+        askName.close();
+    });
+}
+
+nameHero();
